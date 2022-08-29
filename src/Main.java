7ba1aa0d177688905954c49ca0cfc60c1e174c7e
jah <celt8@yandex.ru>
2022-08-29 12:07:20 +0500

    - hm7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+//Exercise 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName+" "+middleName+" "+lastName;
+        System.out.println("ФИО сотрудника - "+fullName);
 
+        //Exercise 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - "+fullName.toUpperCase());
+
+        //Exercise 3
+        String fullName2 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - "+fullName2.replaceAll("ё", "е"));
     }
 }
